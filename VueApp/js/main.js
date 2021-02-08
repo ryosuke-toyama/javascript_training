33a5187a813c4402e5f4dba1cccf29156bc63d36
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2021-02-08 23:24:20 +0900

    いいねボタン設定

--- VueApp/js/main.js
+++ VueApp/js/main.js
@@ -1,8 +1,31 @@
 (function() {
   'use strict';
 
+  const likeCompornent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+    data: function(){
+        return {
+          count: 0
+        }
+      },
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
+    methods: {
+      countUp: function(){
+      this.count++;
+      }
+    }
+  });
+
   const vm = new Vue({
     el: '#app',
+    components: {
+      'like-compornent': likeCompornent
+    },
     data: {
       newItem: '',
       todos: [{
